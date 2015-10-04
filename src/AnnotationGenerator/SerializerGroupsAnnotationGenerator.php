0049f95ee81e8da6374b435a8dcf6c94a9284810
Youssef El Montaser <youssef@elmontaser.com>
2015-10-04 21:29:03 +0200

    change copyright

--- src/AnnotationGenerator/SerializerGroupsAnnotationGenerator.php
+++ src/AnnotationGenerator/SerializerGroupsAnnotationGenerator.php
@@ -1,12 +1,10 @@
 <?php
 
 /*
- * This file is part of the schema-generator.
+ * (c) Kévin Dunglas <dunglas@gmail.com>
  *
- * (c) Youssef El Montaser <youssef@elmontaser.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace ApiPlatform\SchemaGenerator\AnnotationGenerator;
