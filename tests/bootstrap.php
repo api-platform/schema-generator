bd2e57d20ddf3bf02cdbb75580a1ba97c3e59006
Kévin Dunglas <dunglas@gmail.com>
2016-03-05 19:11:27 +0100

    Support annotations of api-patform/core v2

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+return require __DIR__.'/../vendor/autoload.php';
