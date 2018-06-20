22054894e6f5a55ba8f26103b899d872e5476006
Erik Saunier <erik.saunier@gmail.com>
2018-06-21 01:05:31 +0200

    Add support for custom columnPrefix.

---
+++ tests/AnnotationGenerator/DoctrineOrmAnnotationGeneratorTest.php
@@ -0,0 +1,101 @@
+<?php
+
+/*
+ * This file is part of the API Platform project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ApiPlatform\SchemaGenerator\Tests\AnnotationGenerator;
+
+use ApiPlatform\SchemaGenerator\AnnotationGenerator\DoctrineOrmAnnotationGenerator;
+use ApiPlatform\SchemaGenerator\TypesGenerator;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\NullLogger;
+
+/**
+ * @author Erik Saunier <erik.saunier@gmail.com>
+ */
+class DoctrineOrmAnnotationGeneratorTest extends TestCase
+{
+    /**
+     * @var DoctrineOrmAnnotationGenerator
+     */
+    private $generator;
+
+    public function setUp()
+    {
+        $graph = new \EasyRdf_Graph();
+        $myEnum = new \EasyRdf_Resource('https://schema.org/MyEnum', $graph);
+        $myEnum->add('rdfs:subClassOf', ['type' => 'uri', 'value' => TypesGenerator::SCHEMA_ORG_ENUMERATION]);
+        $this->generator = new DoctrineOrmAnnotationGenerator(
+            new NullLogger(),
+            [],
+            [],
+            [],
+            [
+                'Product' => [
+                    'isEnum' => false,
+                    'resource' => new \EasyRdf_Resource('https://schema.org/Product', $graph),
+                    'abstract' => true,
+                    'embeddable' => false,
+                ],
+                'Vehicle' => [
+                    'isEnum' => false,
+                    'abstract' => false,
+                    'embeddable' => false,
+                    'resource' => new \EasyRdf_Resource('https://schema.org/Vehicle', $graph),
+                    'fields' => [
+                        'weight' => [
+                            'isEnum' => false,
+                            'isId' => false,
+                            'isEmbedded' => true,
+                            'range' => 'QuantitativeValue',
+                            'columnPrefix' => false,
+                        ],
+                        'prefixedWeight' => [
+                            'isEnum' => false,
+                            'isId' => false,
+                            'isEmbedded' => true,
+                            'range' => 'QuantitativeValue',
+                            'columnPrefix' => 'weight_',
+                        ],
+                    ],
+                ],
+                'QuantitativeValue' => [
+                    'isEnum' => false,
+                    'abstract' => false,
+                    'name' => 'QuantitativeValue',
+                    'resource' => new \EasyRdf_Resource('https://schema.org/QuantitativeValue', $graph),
+                    'embeddable' => true,
+                ],
+                'MyEnum' => ['isEnum' => true, 'resource' => $myEnum],
+            ]
+        );
+    }
+
+    public function testGenerateClassAnnotations()
+    {
+        $this->assertSame([], $this->generator->generateClassAnnotations('MyEnum'));
+        $this->assertSame(['', '@ORM\MappedSuperclass'], $this->generator->generateClassAnnotations('Product'));
+        $this->assertSame(['', '@ORM\Entity'], $this->generator->generateClassAnnotations('Vehicle'));
+        $this->assertSame(['', '@ORM\Embeddable'], $this->generator->generateClassAnnotations('QuantitativeValue'));
+    }
+
+    public function testGenerateFieldAnnotations()
+    {
+        $this->assertSame(
+            ['@ORM\Embedded(class="QuantitativeValue", columnPrefix=false)'],
+            $this->generator->generateFieldAnnotations('Vehicle', 'weight')
+        );
+        $this->assertSame(
+            ['@ORM\Embedded(class="QuantitativeValue", columnPrefix="weight_")'],
+            $this->generator->generateFieldAnnotations('Vehicle', 'prefixedWeight')
+        );
+    }
+}
