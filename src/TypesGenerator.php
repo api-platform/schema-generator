705d86922384c8990969094abcc50f2064eee437
Nikita Tkalenko <nikita.tkalenko@namecheap.com>
2021-10-19 12:28:52 +0300

    chore: fix phpcs

--- src/TypesGenerator.php
+++ src/TypesGenerator.php
@@ -24,7 +24,6 @@ use ApiPlatform\SchemaGenerator\Model\Class_;
 use ApiPlatform\SchemaGenerator\PropertyGenerator\PropertyGenerator;
 use Doctrine\Inflector\Inflector;
 use EasyRdf\Graph;
-use EasyRdf\Resource;
 use EasyRdf\Resource as RdfResource;
 use PhpCsFixer\Cache\NullCacheManager;
 use PhpCsFixer\Differ\NullDiffer;
